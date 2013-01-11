ba84bfbdea180777d2649fc91bcfe69427d596e3
Jeremy Sevellec <jsevellec@gmail.com>
2013-01-11 09:29:25 +0100

    refactoring to handle easily simple get question

--- src/main/java/Resource.java
+++ src/main/java/Resource.java
@@ -2,25 +2,30 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/")
 public class Resource {
 
+    private Map<String, String> simpleGetResponses = new HashMap<>();
+
+
+    public Resource() {
+        simpleGetResponses.put("Quelle est ton adresse email","tometjerem@gmail.com");
+        simpleGetResponses.put("Es tu abonne a la mailing list(OUI/NON)","OUI");
+        simpleGetResponses.put("Es tu heureux de participer(OUI/NON)","OUI");
+        simpleGetResponses.put("Est ce que tu reponds toujours oui(OUI/NON)","NON");
+        simpleGetResponses.put("ping","OK");
+    }
+
     @GET
     @Produces("text/plain;charset=utf-8")
     public String getAnswer(@QueryParam("q") String question) {
-        if ("Quelle est ton adresse email".equals(question)) {
-            return "tometjerem@gmail.com";
-        } else if ("Es tu abonne a la mailing list(OUI/NON)".equals(question)) {
-            return "OUI";
-        } else if ("Es tu heureux de participer(OUI/NON)".equals(question)) {
-            return "OUI";
-        } else if ("Est ce que tu reponds toujours oui(OUI/NON)".equals(question)) {
-            return "NON";
-        } else if ("ping".equals(question)) {
-            return "OK";
-        } else {
-            return "Vous pouvez répéter la question ?";
+        if (simpleGetResponses.containsKey(question)) {
+            return simpleGetResponses.get(question);
         }
+
+        return "Vous pouvez répéter la question ?";
     }
 }
