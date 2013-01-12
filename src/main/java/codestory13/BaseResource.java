2d9aad2328474cdc3976be43a6a26a49f1cd2baf
Thomas Recloux <thomas.recloux@gmail.com>
2013-01-12 15:10:35 +0100

    Did not have a so bad night

--- src/main/java/codestory13/BaseResource.java
+++ src/main/java/codestory13/BaseResource.java
@@ -58,6 +58,7 @@ public class BaseResource {
         simpleGetResponses.put("As tu bien recu le premier enonce(OUI/NON)","OUI");
         simpleGetResponses.put("ping","OK");
         simpleGetResponses.put("((1,1 2) 3,14 4 (5 6 7) (8 9 10)*4267387833344334647677634)/2*553344300034334349999000","31878018903828899277492024491376690701584023926880");
+        simpleGetResponses.put("As tu passe une bonne nuit malgre les bugs de l etape precedente","BOF");
     }
 
     @GET
