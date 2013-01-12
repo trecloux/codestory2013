935db61518ebb3183f9e8494f7e369d841afcfbd
Thomas Recloux <thomas.recloux@gmail.com>
2013-01-12 14:09:10 +0100

    Try hard coded value to find the spec

--- src/main/java/codestory13/BaseResource.java
+++ src/main/java/codestory13/BaseResource.java
@@ -58,6 +58,7 @@ public class BaseResource {
         simpleGetResponses.put("Est ce que tu reponds toujours oui(OUI/NON)","NON");
         simpleGetResponses.put("As tu bien recu le premier enonce(OUI/NON)","OUI");
         simpleGetResponses.put("ping","OK");
+        simpleGetResponses.put("((1,1+2)+3,14+4+(5+6+7)+(8+9+10)*4267387833344334647677634)/2*553344300034334349999000","3,18780189038289e+49");
     }
 
     @GET
