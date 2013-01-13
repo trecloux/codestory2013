1ced8beb3a9eb6127f1be860c37789482ccba1e9
Thomas Recloux <thomas.recloux@gmail.com>
2013-01-13 20:57:24 +0100

    BaseResource refactoring

--- src/main/java/codestory13/BaseResource.java
+++ src/main/java/codestory13/BaseResource.java
@@ -1,16 +1,20 @@
 package codestory13;
 
 import com.google.common.base.Optional;
-import groovy.lang.GroovyRuntimeException;
 import groovy.lang.GroovyShell;
 import org.codemonkey.simplejavamail.Email;
 import org.codemonkey.simplejavamail.Mailer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.*;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -18,8 +22,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.google.common.base.Optional.absent;
-import static com.google.common.base.Optional.of;
+import static com.google.common.base.Optional.*;
+import static java.lang.System.getenv;
 import static java.util.Locale.FRANCE;
 import static javax.mail.Message.RecipientType.TO;
 import static org.codemonkey.simplejavamail.TransportStrategy.SMTP_TLS;
@@ -28,63 +32,54 @@ import static org.codemonkey.simplejavamail.TransportStrategy.SMTP_TLS;
 public class BaseResource {
 
     private Logger logger = LoggerFactory.getLogger(BaseResource.class);
-    static Mailer mailer;
     static List<String> knownMailKeys = new ArrayList<>();
-    private Map<String, String> simpleGetResponses = new HashMap<>();
-    
 
-    public BaseResource() {
-        initializeSimpleGetResponses();
-        initializeMailer();
+    private static Map<String, String> staticResponses = new HashMap<>();
+    static {
+        staticResponses.put("Quelle est ton adresse email", "tometjerem@gmail.com");
+        staticResponses.put("Es tu abonne a la mailing list(OUI/NON)", "OUI");
+        staticResponses.put("Es tu heureux de participer(OUI/NON)", "OUI");
+        staticResponses.put("Est ce que tu reponds toujours oui(OUI/NON)", "NON");
+        staticResponses.put("As tu bien recu le premier enonce(OUI/NON)", "OUI");
+        staticResponses.put("ping", "OK");
+        staticResponses.put("As tu passe une bonne nuit malgre les bugs de l etape precedente(PAS_TOP/BOF/QUELS_BUGS)", "BOF");
     }
 
-    private void initializeMailer() {
-        if (mailer == null) {
-            String password = System.getenv("GMAIL_PASSWORD");
-            mailer = new Mailer("smtp.gmail.com", 587, "tometjerem@gmail.com", password, SMTP_TLS);
-        }
-    }
-
-    private void initializeSimpleGetResponses() {
-        simpleGetResponses.put("Quelle est ton adresse email","tometjerem@gmail.com");
-        simpleGetResponses.put("Es tu abonne a la mailing list(OUI/NON)","OUI");
-        simpleGetResponses.put("Es tu heureux de participer(OUI/NON)","OUI");
-        simpleGetResponses.put("Est ce que tu reponds toujours oui(OUI/NON)","NON");
-        simpleGetResponses.put("As tu bien recu le premier enonce(OUI/NON)","OUI");
-        simpleGetResponses.put("ping","OK");
-        simpleGetResponses.put("As tu passe une bonne nuit malgre les bugs de l etape precedente(PAS_TOP/BOF/QUELS_BUGS)","BOF");
+    static Mailer mailer;
+    static {
+        mailer = new Mailer("smtp.gmail.com", 587, "tometjerem@gmail.com", getenv("GMAIL_PASSWORD"), SMTP_TLS);
     }
 
     @GET
     @Produces("text/plain;charset=utf-8")
-    public String getAnswer(@QueryParam("q") String question) {
-        if (question != null) {
-            if (simpleGetResponses.containsKey(question)) {
-                return simpleGetResponses.get(question);
-            } else {
-                Optional<String> formulaResponse = formula(question);
-                if (formulaResponse.isPresent()) {
-                    return formulaResponse.get();
-                } else if(isUnknownMailKey("question/" + question)) {
-                    sendEmail("Code Story : Unknown GET question", question);
-                }
-            }
-        }
+    public String getAnswer(@QueryParam("q") @DefaultValue("") String question) {
+        return staticContent(question)
+                .or(formula(question))
+                .or(defaultResponse(question));
+    }
+
+    private Optional<String> staticContent(String question) {
+        return fromNullable(staticResponses.get(question));
+    }
+
+    private String defaultResponse(String question) {
+        sendMailIfNewRequest("question/" + question, "GET question", question);
         return "Vous pouvez répéter la question ?";
     }
+
 
     private Optional<String> formula(String question) {
         String formula = question.replaceAll(" ", "+").replaceAll(",", ".");
         try {
             Object result = new GroovyShell().evaluate(formula);
-            if (result instanceof BigDecimal) {
+            if (result instanceof Number) {
                 NumberFormat format = DecimalFormat.getInstance(FRANCE);
                 format.setGroupingUsed(false);
                 return of(format.format(result));
             } else {
-                return of(result.toString());
+                return absent();
             }
-        } catch (GroovyRuntimeException e) {
+        } catch (Exception e) {
             return absent();
         }
     }
@@ -94,18 +89,14 @@ public class BaseResource {
     @Path("enonce/{subjectNumber}")
     public Response postSubject(@PathParam("subjectNumber")String subjectNumber, String subjectContent) {
         logger.info("énoncé numero {} : {}", subjectNumber,subjectContent);
-        if (isUnknownMailKey("enonce/" + subjectNumber)) {
-            sendEmail("Code Story : Unknown POST Subject", subjectContent);
-        }
+        sendMailIfNewRequest("enonce/" + subjectNumber, "POST Subject", subjectContent);
         return Response.created(null).build();
     }
 
-    private boolean isUnknownMailKey(String key) {
-        if (knownMailKeys.contains(key)) {
-            return false;
-        } else {
-            knownMailKeys.add(key);
-            return true;
+    private void sendMailIfNewRequest(String requestKey, String subject, String body) {
+        if (! knownMailKeys.contains(requestKey)) {
+            sendEmail("Code Story : Unknown " + subject, body);
+            knownMailKeys.add(requestKey);
         }
     }
 
@@ -121,6 +112,5 @@ public class BaseResource {
             logger.error("Error sending email, subject : {}, body : {}", email.getSubject(), email.getText(), e);
         }
     }
-
 
 }
