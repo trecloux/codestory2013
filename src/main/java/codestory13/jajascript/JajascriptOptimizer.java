bcd06928c931f2b7b8098d3d279cfed3cf3bd977
Thomas Recloux <thomas.recloux@gmail.com>
2013-01-15 17:35:30 +0100

    Jajascript polishing

--- src/main/java/codestory13/jajascript/JajascriptOptimizer.java
+++ src/main/java/codestory13/jajascript/JajascriptOptimizer.java
@@ -1,11 +1,9 @@
 package codestory13.jajascript;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static java.lang.Integer.compare;
-import static java.util.Collections.emptySet;
 
 public class JajascriptOptimizer {
     public OrderPath bestPath = null;
@@ -15,24 +13,24 @@ public class JajascriptOptimizer {
         combinePossibleOrderAndKeepTheBestPath(0, orders);
     }
 
-    private Set<OrderPath> combinePossibleOrderAndKeepTheBestPath(int index, List<Order> orders) {
+    private List<OrderPath> combinePossibleOrderAndKeepTheBestPath(int index, List<Order> orders) {
+        List<OrderPath> possiblePaths = newArrayList();
         if (index == orders.size()) {
-            return emptySet();
+            return possiblePaths;
         }
-        Set<OrderPath> possiblePaths = new HashSet<>();
         Order firstOrder = orders.get(index);
-        rememberAddEvalCandidate(new OrderPath(firstOrder), possiblePaths);
-        Set<OrderPath> subPaths = combinePossibleOrderAndKeepTheBestPath(index + 1, orders);
+        rememberAndEvalCandidate(new OrderPath(firstOrder), possiblePaths);
+        List<OrderPath> subPaths = combinePossibleOrderAndKeepTheBestPath(index + 1, orders);
         possiblePaths.addAll(subPaths);
         for (OrderPath subPath : subPaths) {
             if (firstOrder.canBeChainedWith(subPath)) {
-                rememberAddEvalCandidate(new OrderPath(firstOrder, subPath), possiblePaths);
+                rememberAndEvalCandidate(new OrderPath(firstOrder, subPath), possiblePaths);
             }
         }
         return possiblePaths;
     }
 
-    private void rememberAddEvalCandidate(OrderPath path, Set<OrderPath> possiblePaths) {
+    private void rememberAndEvalCandidate(OrderPath path, List<OrderPath> possiblePaths) {
         possiblePaths.add(path);
         if (path.isBetterThan(bestPath)) {
             bestPath = path;
