5e3ff29e0986bdae3cd5775efee8159b1a7db195
Thomas Recloux <thomas.recloux@gmail.com>
2013-01-16 22:23:29 +0100

    Refacor and rename groovy test script

---
+++ perfTest.groovy
@@ -0,0 +1,68 @@
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.6')
+import groovyx.net.http.HTTPBuilder
+
+import static groovyx.net.http.ContentType.JSON
+import static groovyx.net.http.ContentType.URLENC
+import static java.lang.Integer.parseInt
+
+def url = urlParam()
+println("Url : ${url}")
+def size = sizeParam()
+println("Size : ${size}" )
+def payload = buildJsonRequest(size)
+callServer(url, payload)
+
+
+private void callServer(String url, String requestPayload) {
+    def http = new HTTPBuilder(url)
+    def start = System.currentTimeMillis();
+    http.post(path: 'optimize', body: requestPayload.toString(),
+            requestContentType: JSON) { resp, jsonResp ->
+
+        def duration = System.currentTimeMillis() - start
+        println "Response time ${duration} ms"
+        println "Response status: ${resp.statusLine}"
+        //println "Response data : ${jsonResp}"
+        assert resp.statusLine.statusCode == 200
+    }
+}
+
+private String buildJsonRequest(int size) {
+    def random = new Random();
+    def json = new StringBuffer('[')
+    for (int i=0; i < size; i++) {
+        def duration = random.nextInt(10) +1
+        def price = random.nextInt(25) +1
+        json.append('{"VOL":"custom-' + i + '","DEPART":' + i + ',"DUREE":' + duration + ',"PRIX": '+ price +'}')
+        if (i < (size-1)) {
+            json.append(',')
+        }
+    }
+    json.append(']')
+    json.toString();
+}
+
+private int sizeParam() {
+    if (args.length == 0) {
+        return 100000
+    } else if (args.length == 1) {
+        return parseInt(args[0])
+    } else {
+        return parseInt(args[1])
+    }
+}
+
+private String urlParam() {
+    if (args.length < 2 || args[0] == 'heroku') {
+        return 'http://codestory2013.unchticafe.fr/jajascript/'
+    } else if (args[0] == "local") {
+        return "http://localhost:5000/jajascript/"
+    } else if (args[0] == "test") {
+        return 'http://code-story-test.herokuapp.com/jajascript/'
+    } else if (args[0] == "cloudfoundry") {
+        return 'http://codestory-tometjerem.cloudfoundry.com/jajascript/'
+    }
+}
+
+
+
