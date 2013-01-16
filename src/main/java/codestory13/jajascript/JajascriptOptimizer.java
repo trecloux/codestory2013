870e187a7c1a74f077d6181b250ed0ae6c167f34
Thomas Recloux <thomas.recloux@gmail.com>
2013-01-16 14:06:03 +0100

    Log times

--- src/main/java/codestory13/jajascript/JajascriptOptimizer.java
+++ src/main/java/codestory13/jajascript/JajascriptOptimizer.java
@@ -1,5 +1,9 @@
 package codestory13.jajascript;
 
+import com.google.common.base.Stopwatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,14 +11,26 @@ import java.util.Map;
 import static java.lang.Integer.compare;
 
 public class JajascriptOptimizer {
+    private static final Logger log = LoggerFactory.getLogger(JajascriptOptimizer.class);
     public OrderPath bestPath = null;
     private Map<String, OrderPath> bestPathPerOrder = new HashMap<>();
 
     public JajascriptOptimizer(List<Order> orders) {
+        Stopwatch globalwatch = new Stopwatch().start();
+        Stopwatch stopwatch = new Stopwatch().start();
         orders.sort((a, b) -> compare(a.start, b.start));
+        stopwatch.stop();
+        log.info("Sort took {}", stopwatch);
+        stopwatch = new Stopwatch().start();
         for (int i = orders.size() - 1; i >= 0; i--) {
             findBestCombinationStartingWith(i, orders);
+
         }
+        stopwatch.stop();
+        log.info("loop took {}", stopwatch);
+        globalwatch.stop();
+        log.info("optimize took {}", globalwatch);
+
     }
 
     private void findBestCombinationStartingWith(int orderIndex, List<Order> orders) {
