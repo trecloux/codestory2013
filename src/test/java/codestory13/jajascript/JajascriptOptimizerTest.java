1c7071e9e3f184598783612cc9a9a6fe90fdffca
Thomas Recloux <thomas.recloux@gmail.com>
2013-01-18 00:13:29 +0100

    Compare results with old implementation

--- src/test/java/codestory13/jajascript/JajascriptOptimizerTest.java
+++ src/test/java/codestory13/jajascript/JajascriptOptimizerTest.java
@@ -1,11 +1,25 @@
 package codestory13.jajascript;
 
+import com.google.common.base.Stopwatch;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import static java.lang.Integer.compare;
 import static java.util.Arrays.asList;
 import static org.fest.assertions.Assertions.assertThat;
 
 public class JajascriptOptimizerTest {
+
+    private static final Logger log = LoggerFactory.getLogger(JajascriptOptimizerTest.class);
+    static Random random = new Random();
+
 
     @Test
     public void should_optimize_with_one_order() throws Exception {
@@ -60,6 +74,34 @@ public class JajascriptOptimizerTest {
         assertThatPathIsAsExpected(bestPath, 18, "MONAD42", "LEGACY01");
     }
 
+    @Test
+    public void should_have_same_results_than_old_impl() throws Exception {
+        Stopwatch oldImplWatch = new Stopwatch();
+        Stopwatch newImplWatch = new Stopwatch();
+        for (int i=1; i<=20; i++) {
+            List<Order> orders = randomOrders(random.nextInt(10_000));
+            oldImplWatch.start();
+            OrderPath bestPathOld = new JavascriptOldOptimizer(new ArrayList(orders)).bestPath;
+            oldImplWatch.stop();
+            newImplWatch.start();
+            OrderPath bestPathNew = new JajascriptOptimizer(new ArrayList<>(orders)).bestPath;
+            newImplWatch.stop();
+            assertThat(bestPathNew.gain).isEqualTo(bestPathOld.gain);
+            assertThat(bestPathNew.getPath()).isEqualTo(bestPathOld.getPath());
+        }
+        double gain = 100* (oldImplWatch.elapsedMillis() - newImplWatch.elapsedMillis()) / (double)oldImplWatch.elapsedMillis();
+        log.info("Gain : {}%", gain);
+    }
+
+    private List<Order> randomOrders(int size) {
+        List<Order> orders = new ArrayList<>();
+        for (int i=0;i<size; i++) {
+            orders.add(new Order("custom-" + i, random.nextInt(size)+1, random.nextInt(10)+1, random.nextInt(20)+1));
+        }
+        orders.sort((a,b) -> Integer.compare(a.start, b.start));
+        return orders;
+    }
+
     private OrderPath optimize(Order ... orders) {
         return new JajascriptOptimizer(asList(orders)).bestPath;
     }
@@ -68,5 +110,40 @@ public class JajascriptOptimizerTest {
     private void assertThatPathIsAsExpected(OrderPath orderPath, int expectedGain, String... path) {
         assertThat(orderPath.gain).isEqualTo(expectedGain);
         assertThat(orderPath.getPath()).containsExactly(path);
+    }
+
+    public static class JavascriptOldOptimizer {
+        public OrderPath bestPath = null;
+        private Map<String, OrderPath> bestPathPerOrder = new HashMap<>();
+
+        public JavascriptOldOptimizer(List<Order> orders) {
+            Stopwatch stopwatch = new Stopwatch().start();
+            orders.sort((a, b) -> compare(a.start, b.start));
+            for (int i = orders.size() - 1; i >= 0; i--) {
+                findBestCombinationStartingWith(i, orders);
+            }
+            log.info("Old optimize took {}", stopwatch);
+
+        }
+
+        private void findBestCombinationStartingWith(int orderIndex, List<Order> orders) {
+            Order startingOrder = orders.get(orderIndex);
+            OrderPath bestCombinablePath = null;
+            for (int i = orders.size() -1; i > orderIndex; i--) {
+                OrderPath orderPath = bestPathPerOrder.get(orders.get(i).flight);
+                if (orderPath.canStartWith(startingOrder)) {
+                    if (orderPath.isBetterThan(bestCombinablePath)) {
+                        bestCombinablePath = orderPath;
+                    }
+                } else {
+                    break;
+                }
+            }
+            OrderPath bestCombinationIncludingStartingOrder = new OrderPath(startingOrder, bestCombinablePath);
+            bestPathPerOrder.put(startingOrder.flight, bestCombinationIncludingStartingOrder);
+            if (bestCombinationIncludingStartingOrder.isBetterThan(bestPath)) {
+                bestPath = bestCombinationIncludingStartingOrder;
+            }
+        }
     }
 }
